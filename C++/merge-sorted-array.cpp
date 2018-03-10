900cbd537fd40277f526fb7576ae09b64f4e0104
houmin <weihoumin@gmail.com>
2018-03-10 14:08:31 +0800

    merge sorted array.

---
+++ C++/merge-sorted-array.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2018 all rights reserved.
+ *
+ * Author: Houmin Wei <houmin.wei@outlook.com>
+ *
+ * Source: https://leetcode.com/problems/merge-sorted-array
+ *
+ * Description:
+ *  Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ */
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1, j = n - 1, tar = m + n - 1;
+        while (j >= 0) {
+            nums1[tar--] = i >= 0 && nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+        }
+    }
+};
