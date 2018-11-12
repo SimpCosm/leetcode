84cabc8325de234bf996daf29bab73886b17a67a
houmin <weihoumin@gmail.com>
2018-11-12 21:17:08 +0800

    FEA: finish construct binary tree from preorder and inorder traversal.

---
+++ C++/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2018 All Rights Reserved.
+ *
+ * Author: Houmin Wei <houmin.wei@outlook.com>
+ *
+ * Source: https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal
+ *
+ */
+
+#include <vector>
+#include <stack>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+
+public:
+    /* from Preorder and Inorder Traversal */
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return helper(preorder,0,preorder.size(),inorder,0,inorder.size());
+    }
+
+    TreeNode* helper(vector<int>& preorder,int i,int j,vector<int>& inorder,int ii,int jj)
+    {
+        if(i >= j || ii >= j)
+            return NULL;
+
+        int mid = preorder[i];
+        auto f = find(inorder.begin() + ii,inorder.begin() + jj,mid);
+
+        int dis = f - inorder.begin() - ii;
+
+        TreeNode* root = new TreeNode(mid);
+        root -> left = helper(preorder,i + 1,i + 1 + dis,inorder,ii,ii + dis);
+        root -> right = helper(preorder,i + 1 + dis,j,inorder,ii + dis + 1,jj);
+        return root;
+    }
+};
+
