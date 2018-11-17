d97d37f94d3ca1ad6ff00babef51470a9be916b0
houmin <weihoumin@gmail.com>
2018-11-17 13:51:34 +0800

    FEA: rename file name

---
+++ C++/leetcode0322-coin-change.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2018 all rights reserved.
+ *
+ * Author: Houmin Wei <houmin.wei@outlook.com>
+ *
+ * Source: https://leetcode.com/problems/coin-changes
+ */
+
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<vector<int>> f(m+1, vector<int>(n+1, 0));
+        f[0][1] = 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (obstacleGrid[i-1][j-1] == 0) {
+                    f[i][j] = f[i-1][j] + f[i][j-1];
+                }
+            }
+        }
+        return f[m][n];
+    }
+};
